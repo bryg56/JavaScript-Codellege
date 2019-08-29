2829e5b8ffda43a588dfc7e68bda25aa81c1d879
bryg56 <jodido34.lp@gmail.com>
2019-08-29 12:30:22 -0500

    Practica una de sumar dos numeros. First commit

---
+++ Ejemplo1/app.js
@@ -0,0 +1,12 @@
+let btnCalcula = document.getElementById('btnCalcula');
+btnCalcula.addEventListener('click', suma);
+
+function suma() {
+    Numero1 = Numero1 + Numero2;
+    console.log(Numero1);
+}
+// consola();
+
+function consola() {
+    console.log('xyz');
+}
