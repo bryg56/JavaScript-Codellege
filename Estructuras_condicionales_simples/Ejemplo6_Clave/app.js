a794e759766df1a5c437aa4f7bd463e1395fd15f
bryg56 <jodido34.lp@gmail.com>
2019-09-05 09:30:12 -0500

    Practica 6 a medias, falta terminar mas cosas

---
+++ Estructuras_condicionales_simples/Ejemplo6_Clave/app.js
@@ -0,0 +1,12 @@
+let bton_c = document.getElementById('calc');
+let imprime = document.getElementsById('resul');
+event();
+
+function event() {
+    bton_c.addEventListener('click', Clave);
+}
+
+function Clave() {
+    let cla = document.getElementsById('clave');
+
+}
