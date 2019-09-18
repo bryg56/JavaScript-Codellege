1128b32ddd10371ee35b31c595f185f4df83a6f3
bryg56 <jodido34.lp@gmail.com>
2019-09-18 12:52:46 -0500

    Terminado ahora si, bueno no, me faltan dos, solo terminado el 3 y el de las fabricas a medias.

--- Estructuras_ciclicas/SeisDias/app.js
+++ Estructuras_ciclicas/SeisDias/app.js
@@ -1,78 +1,44 @@
-let btnAgrega = document.getElementById('agrega');
 let btnCalcula = document.getElementById('calcula');
+let btnAgrega = document.getElementById('agrega');
 let contador = 1;
+let impresora = document.getElementById('resultado');
 event();
 
 function event() {
-    btnAgrega.addEventListener('click', AgregaT);
+    btnAgrega.addEventListener('click', agrega);
+    btnCalcula.addEventListener('click', calcular);
 }
 
-function AgregaT() {
+function agrega() {
     contador++;
-    let divRowL = document.createElement('div');
-    divRowL.setAttribute("class", "row");
-    let divColL = document.createElement('div');
-    divColL.setAttribute('class', 'col-12');
-    let label1 = document.createElement('label');
-    label1.appendChild(document.createTextNode(`Trabajador${contador}`));
-    let Contenedor = document.getElementById('Trabajadores');
-    Contenedor.appendChild(divRowL);
-    divRowL.appendChild(label1);
     let divRow = document.createElement('div');
-    let divCol1 = document.createElement('div');
-    divCol1.setAttribute("class", "col-2");
-    let divForm1 = document.createElement('div');
-    divForm1.setAttribute('class', 'form-group');
-    let label1 = document.createElement('label');
-    label1.appendChild(document.createTextNode("Dia1"));
-    let divInput1 = document.createElement('input');
-    divInput1.setAttribute('id', `diaTrabajador${contador}`);
-    let divCol2 = document.createElement('div');
-    divCol2.setAttribute("class", "col-2");
-    let divForm1 = document.createElement('div');
-    divForm1.setAttribute('class', 'form-group');
-    let label1 = document.createElement('label');
-    label1.appendChild(document.createTextNode("Dia1"));
-    let divInput1 = document.createElement('input');
-    divInput1.setAttribute('id', `diaTrabajador${contador}`);
-    let divCol3 = document.createElement('div');
-    divCol3.setAttribute("class", "col-2");
-    let divForm1 = document.createElement('div');
-    divForm1.setAttribute('class', 'form-group');
-    let label1 = document.createElement('label');
-    label1.appendChild(document.createTextNode("Dia1"));
-    let divInput1 = document.createElement('input');
-    divInput1.setAttribute('id', `diaTrabajador${contador}`);
-    let divCol4 = document.createElement('div');
-    divCol4.setAttribute("class", "col-2");
-    let divForm1 = document.createElement('div');
-    divForm1.setAttribute('class', 'form-group');
-    let label1 = document.createElement('label');
-    label1.appendChild(document.createTextNode("Dia1"));
-    let divInput1 = document.createElement('input');
-    divInput1.setAttribute('id', `diaTrabajador${contador}`);
-    let divCol5 = document.createElement('div');
-    divCol5.setAttribute("class", "col-2");
-    let divForm1 = document.createElement('div');
-    divForm1.setAttribute('class', 'form-group');
-    let label1 = document.createElement('label');
-    label1.appendChild(document.createTextNode("Dia1"));
-    let divInput1 = document.createElement('input');
-    divInput1.setAttribute('id', `diaTrabajador${contador}`);
-    let divCol6 = document.createElement('div');
-    divCol6.setAttribute("class", "col-2");
-    let divForm1 = document.createElement('div');
-    divForm1.setAttribute('class', 'form-group');
-    let label1 = document.createElement('label');
-    label1.appendChild(document.createTextNode("Dia1"));
-    let divInput1 = document.createElement('input');
-    divInput1.setAttribute('id', `diaTrabajador${contador}`);
+    divRow.setAttribute('class', 'row');
+    let divCol = document.createElement('div');
+    divCol.setAttribute('class', 'col-12');
+    let divForm = document.createElement('div');
+    divForm.setAttribute('class', 'form-group');
+    let label = document.createElement('label');
+    label.appendChild(document.createTextNode(`Dia${contador}`));
+    let input = document.createElement('input');
+    input.setAttribute('class', 'form-control');
+    input.setAttribute('id', `diaTrabajador${contador}`);
+    input.setAttribute('input', 'number');
+    let trabajadores = document.getElementById('Trabajadores');
+    trabajadores.appendChild(divRow);
+    divRow.appendChild(divCol);
+    divCol.appendChild(divForm);
+    divForm.appendChild(label);
+    divForm.appendChild(input);
+}
 
-
-
-
-
-
-
-
+function calcular() {
+    let paHora = Number(document.getElementById('costoHora').value);
+    let Total = 0;
+    let Paga = 0;
+    for (let i = 1; i <= contador; i++) {
+        let dia = Number(document.getElementById(`diaTrabajador${i}`).value);
+        Total += dia;
+        Paga += (paHora * dia);
+    }
+    impresora.innerHTML = `Horas totales=${Total} <br> Paga total = ${Paga}`;
 }
